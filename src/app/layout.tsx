c0bb7e6992f23f1604ec89a74f60723f35898598
BRian <yusuffebrian1004@gmail.com>
2026-05-31 21:52:12 +0700

    feat(metadata): add paw print favicon to application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ const fredoka = Fredoka({
 export const metadata: Metadata = {
   title: "Pet Quiz Game",
   description: "Level up your skills and take care of your pet!",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🐾</text></svg>",
+  },
 };
 
 export default function RootLayout({
